9c76bafd30948e44eb12233802c0b63d61fb2f50
AnastasiaVelychko <129153930+AnastasiaVelychko@users.noreply.github.com>
2025-04-25 11:17:08 +0300

    Added LR2

--- lab2/Products.Common/Entities/Customer.cs
+++ lab2/Products.Common/Entities/Customer.cs
@@ -18,5 +18,11 @@ namespace Products.Common.Entities
             Email = email;
             Address = address;
         }
+
+        public static Customer Create() 
+            => new Customer(Guid.NewGuid(), string.Empty, string.Empty, string.Empty);
+        
+        public static Customer Create(string name, string email, string address)
+            => new Customer(Guid.NewGuid(), name, email, address);
     }
 }
